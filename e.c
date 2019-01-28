2ddb5a8b15ef3617d5704b854414d40547179674
Alexander Kraus <nr4@z10.info>
2019-01-28 17:25:14 +0100

    sequence texture is now correctly included into gfx shader

--- e.c
+++ e.c
@@ -568,7 +568,7 @@ int main(int argc, char **args)
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, sequence_texture_handle);
         
-        glActiveTexture(GL_TEXTURE1);
+//         glActiveTexture(GL_TEXTURE1);
         
         glBegin(GL_QUADS);
         glVertex3f(-1,-1,0);
